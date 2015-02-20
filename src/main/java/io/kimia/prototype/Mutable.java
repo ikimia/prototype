80f588da76334dfb58f5d49a32b69d844ddaee37
Itay Kimia <ikimia@gmail.com>
2015-02-21 01:57:40 +0200

    fixed Mutable documentation

--- src/main/java/io/kimia/prototype/Mutable.java
+++ src/main/java/io/kimia/prototype/Mutable.java
@@ -4,12 +4,18 @@ import java.lang.annotation.*;
 
 /**
  * <p>
- * Annotates a prototype class member to produce a mutable member in the concrete class.
+ * Annotates a prototype definition to produce all members in the concrete class mutable
+ * or a prototype member to produce a mutable member in the concrete class.
  * </p>
  *
  * <p>
  * Generated members as a result of this annotation will be declared {@code private}
- * instead of {@code private final} and setters will be created for them.
+ * instead of {@code private final} and setters will be created for them. In addition, mutable
+ * members are not added to the generated constructor.
+ * </p>
+ *
+ * <p>
+ * Type mutability can be reverted for a specific field by annotating it as {@code &#064;Mutable(false)}.
  * </p>
  *
  * @see Prototype
